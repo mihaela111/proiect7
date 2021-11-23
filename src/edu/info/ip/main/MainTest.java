cbb3cd5f449a2f0a676fed0fa65a8092becf75e9
mihaela111 <mihaela.pasca@ulbsibiu.ro>
2021-11-23 15:23:49 +0200

    varianta grafica01

--- src/edu/info/ip/main/MainTest.java
+++ src/edu/info/ip/main/MainTest.java
@@ -15,14 +15,10 @@ public class MainTest {
     public static void main(String[] args) {
 
        BufferedImage inputImg= loadImage("./test_images/lena_gray_512.bmp");
-       // displayImage(inputImg, "Original image");
-
-       // displayImage(contrast(inputImg,1.5f), "Contrast");
 
 
 
-       // displayImage(contrastExpandare(inputImg, 3), "Contrast expandare");
-      //  BufferedImage testImage = applySettingsDlg(inputImg, new BrightnessRGBDlg());
+
         BufferedImage testImage = applySettingsDlg(inputImg, new ContrastExpandDlg());
         displayImage(testImage, "Contrast Expandare");
        BufferedImage testImagec = applySettingsDlg(inputImg, new ContrastCompressDlg());
